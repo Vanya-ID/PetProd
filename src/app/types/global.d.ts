6c7fc0a5ff0e60e0cf6924bced9aacbc506ae44f
Ivan Filipchyk <wot.vania@mail.ru>
2023-07-25 19:35:46 +0300

    Added i18m

--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -11,7 +11,8 @@ declare module "*.svg" {
     const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
     export default content;
 }
-
 declare module "*.png";
 declare module "*.jpg";
 declare module "*.jpeg";
+
+declare const __IS_DEV__: boolean;
