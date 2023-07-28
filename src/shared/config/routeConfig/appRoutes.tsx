708540bb011a88bccbd611d5a3ac778153bf8d9f
Ivan Filipchyk <wot.vania@mail.ru>
2023-07-29 00:30:20 +0300

    Added PageLoader and NotFound page

--- src/shared/config/routeConfig/appRoutes.tsx
+++ src/shared/config/routeConfig/appRoutes.tsx
@@ -1,15 +1,18 @@
 import { RouteProps } from 'react-router-dom';
 import { AboutPage } from 'pages/AboutPage';
 import { MainPage } from 'pages/MainPage';
+import { NotFoundPage } from 'pages/NotFoundPage';
 
 export enum AppRoutes {
     MAIN = 'main',
     ABOUT = 'about',
+    NOT_FOUND = 'not_found',
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
     [AppRoutes.MAIN]: '/',
     [AppRoutes.ABOUT]: '/about',
+    [AppRoutes.NOT_FOUND]: '*',
 };
 
 export const routeConfig: Record<AppRoutes, RouteProps> = {
@@ -20,5 +23,9 @@ export const routeConfig: Record<AppRoutes, RouteProps> = {
     [AppRoutes.MAIN]: {
         path: RoutePath.main,
         element: <MainPage />,
+    },
+    [AppRoutes.NOT_FOUND]: {
+        path: RoutePath.main,
+        element: <NotFoundPage />,
     },
 };
